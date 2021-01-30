17b3e746a73bdd4c11e2f25083694c32e53d952e
EC2 Default User <ec2-user@ip-172-31-11-209.us-east-2.compute.internal>
2021-01-30 06:46:20 +0000

    add Goal.vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<goals-component></goals-component>
+@endsection
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
