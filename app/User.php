d3caa341e0d0fc9cfe6ceb558c7db530d6e08209
EC2 Default User <ec2-user@ip-172-31-11-209.us-east-2.compute.internal>
2021-01-30 07:06:39 +0000

    impl relation

--- app/User.php
+++ app/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Todo');
     }
+    
+    public function tags()
+    {
+        return $this->hasMany('App\Tag');
+    }
 }
